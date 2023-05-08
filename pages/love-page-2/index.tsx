f99c14dc393ce7f8836fc8b1d9ae449d50ea018c
bCatanant <bruno.catanant@zup.com.br>
2023-05-08 02:11:39 -0300

    pedido page 3
    Signed-off-by: bCatanant <bruno.catanant@zup.com.br>

--- pages/love-page-2/index.tsx
+++ pages/love-page-2/index.tsx
@@ -8,31 +8,29 @@ export default function Home() {
     <div className={utilStyles.containerText}>
       <section className={utilStyles.headingMd}>
         <p>
-          aqui jas textasda aqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasda aqui jas textasda aqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasdaaqui jas textasdaaqui jas textasdaaqui jas textasdaaqui jas
-          textasda
+          Já que fez a escolha certa, pq eu te dei muita a opção do não neh
+          ashhsahsad, tala na tela pelo menos.
         </p>
+        <p>
+          Mas brincadeiras a parte, eu to muito feliz que ter feito isso, é a
+          primeira vez que faço algo assim e to muito feliz em ter feito isso
+          para te pedir em namoro, e vou fica mais feliz ainda se tu aceitar.
+        </p>
+        <p>
+          Tem certeza que quer namorar comigo, e olha que eu quero passar e
+          construir uma vida ao seu lado?
+        </p>
+      </section>
+
+      <section
+        className={`${utilStyles.headingMdPg2} ${utilStyles.padding1px}`}
+      >
+        <button
+          className={`${utilStyles.buttomPg2}`}
+          onClick={() => router.push('/love-page-3')}
+        >
+          sim, eu tenho certeza
+        </button>
       </section>
     </div>
   );
---
+++ pages/love-page-3/index.tsx
@@ -0,0 +1,31 @@
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <div className={utilStyles.containerText}>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Te amo meu anjo, tu e o sol do meu dia a dia, eu sou muito grato por
+          poder ter você na minha vida e poder contruir uma vida ao seu lado
+          sempre buscando o nosso melhor. Cada dia que passa eu sou mais grato e
+          mais feliz por estar com voce e estar junto a você. Queor te ver todo
+          dias, e sempre quando te vejo e a melhro parte do meu dia.
+        </p>
+        <p>
+          Quero que saiba que a gente e a minha prioridade e logo depois vem
+          voce, eu sempre estarei presente na sua vida e nao vou me afastar de
+          voce. Quero que saiba que sou muito feliz e sou muito apaixonado por
+          vocÊ, eu nunca amei alguem assim, eu nunca voltei para a vida de
+          alguem, eu sou seu lais estou aos seus pes e te AMO muito. To com medo
+          deste sentimento, mas ao mesmo tempo esse snetimento me deixa muito
+          feliz snetir isso por alguem me faz muito feliz.
+        </p>
+        <p>
+          Vou te mimar muito e cuidar de voce um vida toda se deixar. Sou muito
+          grato por sentir isso por voce e saber que nao estou sozinho neste
+          sentimento. Te Amo Lais Tu e o amor da minha vida.
+        </p>
+      </section>
+    </div>
+  );
+}
